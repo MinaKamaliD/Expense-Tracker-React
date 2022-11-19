a06e2da8bbad48f2cfe6a398fae7fe613b65da48
Mina <minakamali.d@gmail.com>
2022-11-19 08:53:12 +0330

    css files modified

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -4,20 +4,11 @@ function ExpenseItem(props) {
   <p className="box__strt">Let's get started!</p>;
 
   return (
-    <div>
-      <div className="box">
-        <div className="contaner__title"></div>
-        <div className="container">
-          <div className="container--items">
-            <div className="container-items__des">
-              <DateItem date={props.date}></DateItem>
-              <p className="box__title">{props.title}</p>
-            </div>
-            <div className="container-items__price">
-              <div className="box__price">${props.price}</div>
-            </div>
-          </div>
-        </div>
+    <div className="expense-item">
+      <DateItem date={props.date}></DateItem>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.price}</div>
       </div>
     </div>
   );
